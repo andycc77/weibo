404b40fdbdcc23cc3d575b1ad2f880a0ca337a13
ChenAllen <andycc77e@gmail.com>
2021-06-03 00:01:31 +0800

    基礎頁面

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    public function home(): string
+    {
+        return view('static_pages.home');
+    }
+
+    public function help(): string
+    {
+        return view('static_pages.help');
+    }
+
+    public function about(): string
+    {
+        return view('static_pages.about');
+    }
+}
